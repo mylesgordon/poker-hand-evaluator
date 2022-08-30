8237e34f28b53a95ac72f8f2144dde10dbb54eda
Myles Gordon <myles.gordon@sky.uk>
2022-08-30 11:16:35 +0100

    Made workshop minimal branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,7 @@
-#include "deck.h"
-#include "hand.h"
 #include <iostream>
 
 int main()
 {
-    bool should_run { true };
-    while (should_run) {
-        Hand hand;
-        auto evaluation = hand.evalulate();
-        std::cout << evaluation << '\n';
-
-        if (evaluation != "None") {
-            should_run = false;
-        }
-    }
-
+    // insert stuff
     return 0;
 }
